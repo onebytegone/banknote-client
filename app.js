41d79243f8da5e0f7de4c4ff7ed87c1fc40f4bbd
Ethan Smith <ethan@onebytegone.com>
2015-05-02 07:14:02 -0400

    Updated to cleaner use of marionette

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
 var _ = require('underscore');
 var Marionette = require('backbone.marionette');
-var $ = require('jquery');
 var TableCollection = require('./src/model/TableCollection');
 
 
-var ListView = Marionette.ItemView.extend({
-   initialize: function(){
-      _.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
-
-      var tables = new TableCollection();
-
-      var that = this;
-      tables.fetch({
-        success: function () {
-            console.log(tables.toJSON());
-            that.render();
-        }
-      });
-   },
-
-   render: function(){
-      $(this.el).append("<ul> <li>hello world</li> </ul>");
-   }
+var ListItem = Marionette.ItemView.extend({
+   template: "#template-slice"
 });
 
 
-var AppLayoutView = Marionette.LayoutView.extend({
-   template: "#layout",
+var ListView = Marionette.CompositeView.extend({
+   template: '#template-itemlist',
+   childView: ListItem,
+   childViewContainer: '#items'
+});
+
+
+var PageLayout = Marionette.LayoutView.extend({
+   template: "#template-main",
 
    regions: {
       elements: "#elements"
    },
 
    onBeforeShow: function() {
-      this.showChildView('elements', new ListView());
+      var tables = new TableCollection();
+      this.showChildView('elements', new ListView({
+         collection: tables
+      }));
+      tables.fetch();
    }
 });
 
@@ -45,7 +39,7 @@ app.addRegions({
 });
 
 app.addInitializer(function(options) {
-    app.appRegion.show(new AppLayoutView());
+    app.appRegion.show(new PageLayout());
 });
 
 app.start();
