ee103085e829a5c10beeb6657a54ff59a3947d7c
Ethan Smith <ethan@onebytegone.com>
2015-05-07 17:26:54 -0400

    Added serializeData override

--- src/view/ListItem.js
+++ src/view/ListItem.js
@@ -1,7 +1,11 @@
 var Marionette = require('backbone.marionette');
 
 var ListItem = Marionette.ItemView.extend({
-   template: "#template-slice"
+   template: "#template-slice",
+   serializeData: function () {
+      console.log(this.model.toJSON());
+      return this.model.toJSON();
+   }
 });
 
 module.exports = ListItem;
