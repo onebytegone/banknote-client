0e6b44181db7399d00526eb503d51d81f7836130
Ethan Smith <ethan@onebytegone.com>
2015-07-26 11:27:46 -0400

    Added yearly total calculation

--- src/model/AmountEntryCollection.js
+++ src/model/AmountEntryCollection.js
@@ -1,8 +1,17 @@
 var Backbone = require('backbone'),
-    AmountEntry = require('./AmountEntry');
+    AmountEntry = require('./AmountEntry'),
+    MoneyStack = require('moneystack');
 
 var AmountEntryCollection = Backbone.Collection.extend({
-   model: AmountEntry
+   model: AmountEntry,
+
+   yearlyTotal: function() {
+      var value = this.reduce(function(carry, entry) {
+         return carry.plus(entry.get('amount'));
+      }, new MoneyStack(0));
+
+      return value;
+   }
 });
 
 module.exports = AmountEntryCollection;
