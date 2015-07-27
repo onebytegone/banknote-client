5ad59dca566754c0871737f5de8f283486517b6f
Ethan Smith <ethan@onebytegone.com>
2015-07-27 12:59:40 -0400

    Moved to using template helper for months

--- src/view/table/MonthlyTable.js
+++ src/view/table/MonthlyTable.js
@@ -4,6 +4,25 @@ var TableBones = require('./TableBones'),
 var MonthlyTable = TableBones.extend({
    template: '#template-monthlytable',
    childView: ListItem,
+
+   templateHelpers: function () {
+      return {
+         months: [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+         ]
+      };
+   }
 });
 
 module.exports = MonthlyTable;
