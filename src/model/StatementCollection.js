39ca7a6c898ab489c6dc35f8f540d97a573d77f5
Ethan Smith <ethan@onebytegone.com>
2015-07-31 20:41:19 -0400

    Added StatementCollection

---
+++ src/model/StatementCollection.js
@@ -0,0 +1,27 @@
+var Backbone = require('backbone'),
+    _ = require('underscore'),
+    Statement = require('./Statement');
+
+var StatementCollection = Backbone.Collection.extend({
+   model: Statement
+}, {
+   generateFromAmountEntryCollection: function(entryCollection, filter) {
+      var collectionList = entryCollection.collectionsByFilter(filter);
+      return _.reduce(collectionList, function(carry, collection, key) {
+         carry.add(new Statement({
+            'key': key,
+            'collection': collection
+         }));
+
+         return carry;
+      }, new StatementCollection());
+   },
+
+   divideByCategories: function(entryCollection) {
+      return this.generateFromAmountEntryCollection(entryCollection, function(entry) {
+         return entry.get('category');
+      });
+   }
+});
+
+module.exports = StatementCollection;
