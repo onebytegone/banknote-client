21f8ccab14e6a3fdcdb69ba72797ce686c162e1c
Ethan Smith <ethan@onebytegone.com>
2015-08-04 20:57:20 -0400

    Whitespace

--- src/view/table/rows/MonthlyTableRow.js
+++ src/view/table/rows/MonthlyTableRow.js
@@ -1,8 +1,8 @@
 var Marionette = require('backbone.marionette');
 
 var MonthlyTableRow = Marionette.ItemView.extend({
-  template: '#template-monthlytablerow',
-  tagName: 'tr'
+   template: '#template-monthlytablerow',
+   tagName: 'tr'
 });
 
 module.exports = MonthlyTableRow;
