b68ec1d7a4743a65ee7919c0f20e13016008501f
Ethan Smith <ethan@onebytegone.com>
2015-08-04 21:11:16 -0400

    Added styles for zero value

--- src/view/table/rows/MonthlyTableRow.js
+++ src/view/table/rows/MonthlyTableRow.js
@@ -2,7 +2,14 @@ var Marionette = require('backbone.marionette');
 
 var MonthlyTableRow = Marionette.ItemView.extend({
    template: '#template-monthlytablerow',
-   tagName: 'tr'
+   tagName: 'tr',
+   templateHelpers: function() {
+      return {
+         addClassByAmount: function(amount) {
+            return amount.get() === 0 ? 'zero' : '';
+         }
+      };
+   }
 });
 
 module.exports = MonthlyTableRow;
