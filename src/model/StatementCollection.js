4f14f31fc3bad2436e8b6d40271a63fe48b1fbbf
Ethan Smith <ethan@onebytegone.com>
2015-08-07 23:12:24 -0400

    Have submodels update when source updates

--- src/model/StatementCollection.js
+++ src/model/StatementCollection.js
@@ -3,18 +3,37 @@ var Backbone = require('backbone'),
     Statement = require('./Statement');
 
 var StatementCollection = Backbone.Collection.extend({
-   model: Statement
-}, {
-   generateFromAmountEntryCollection: function(entryCollection, filter) {
-      var collectionList = entryCollection.collectionsByFilter(filter);
-      return _.reduce(collectionList, function(carry, collection, key) {
+   model: Statement,
+   rebuildDataFromEntryCollection: function(entryCollection, filter) {
+      var self = this,
+          collectionList,
+          updateCollection = function() {
+             self.rebuildDataFromEntryCollection(entryCollection, filter);
+          };
+
+      this.reset();
+
+      // rebuild data
+      collectionList = entryCollection.collectionsByFilter(filter);
+      _.reduce(collectionList, function(carry, collection, key) {
          carry.add(new Statement({
             'key': key,
             'entries': collection
          }));
 
          return carry;
-      }, new StatementCollection());
+      }, this);
+
+      // bind to collection for updates
+      entryCollection.on('change', updateCollection);
+      entryCollection.on('remove', updateCollection);
+      entryCollection.on('add', updateCollection);
+   }
+}, {
+   generateFromAmountEntryCollection: function(entryCollection, filter) {
+      var collection = new StatementCollection();
+      collection.rebuildDataFromEntryCollection(entryCollection, filter);
+      return collection;
    },
 
    divideByCategories: function(entryCollection) {
