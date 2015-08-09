5a94f59bee6141a3f5dc51de4007365667e64bac
Ethan Smith <ethan@onebytegone.com>
2015-08-09 11:27:51 -0400

    Moved monthlySummary to TotalByMonth

---
+++ src/model/operations/entries/TotalByMonth.js
@@ -0,0 +1,66 @@
+var _ = require('underscore'),
+    AmountEntry = require('../../AmountEntry'),
+    AmountEntryCollection = require('../../AmountEntryCollection'),
+    StatementsByFilter = require('../statements/StatementsByFilter');
+
+var TotalByMonth = function() { };
+
+TotalByMonth.prototype = {
+   /**
+    * Creates a collection with a entry for each month. The
+    * value in the entry is the sum of the amounts for that
+    * month. The date will be the first day of the month.
+    *
+    * @param entries AmountEntryCollection
+    * @return AmountEntryCollection
+    */
+   run: function(entries) {
+      var output = new AmountEntryCollection();
+
+      var monthlyStatements = (new StatementsByFilter()).run(entries, function(entry) {
+         return entry.getDateOfMonth();
+      });
+
+      var summedEntries = monthlyStatements.map(function(statement) {
+         return new AmountEntry({
+            'date': statement.get('key'),
+            'amount': statement.get('entries').sumEntries()
+         });
+      });
+
+      var getLastMonthListed = function (entries) {
+         var lastItem = _.last(entries);
+         return lastItem ? lastItem.getMonth() : 0;
+      };
+
+      /**
+       * `startingMonth` and `endingMonth` are inclusive
+       */
+      var generateMissingMonths = function (startingMonth, endingMonth) {
+         return _.map(_.range(startingMonth, endingMonth+1), function (month) {
+            return new AmountEntry({
+               'date': month + '/1'
+            });
+         });
+      };
+
+      // Fill in missing months
+      var filledSet = _.reduce(summedEntries, function(carry, entry) {
+         var missingMonths = generateMissingMonths(getLastMonthListed(carry)+1, entry.getMonth()-1);
+         carry = carry.concat(missingMonths);
+
+         carry.push(entry);
+         return carry;
+      }, []);
+
+      if (filledSet.length < 12) {
+         filledSet = filledSet.concat(generateMissingMonths(getLastMonthListed(filledSet)+1, 12));
+      }
+
+      output.add(filledSet);
+
+      return output;
+   }
+};
+
+module.exports = TotalByMonth;
--- tests/model/AmountEntryCollection.test.js
+++ tests/model/AmountEntryCollection.test.js
@@ -28,33 +28,6 @@ describe('AmountEntryCollection', function() {
       }
    ]);
 
-   it('should return a properly filtered set', function() {
-      var output = basicList.collectionsByFilter(function(entry) {
-         var category = entry.get('category');
-         if (category === 'notused') {
-            return null;
-         }
-
-         return category;
-      });
-
-      //TODO: This needs better testing. Currently difference in _byId prevents a straight compare.
-      expect(_.size(output)).to.be(2);
-      expect(output['first'].length).to.be(2);
-      expect(output['second'].length).to.be(1);
-   });
-
-   it('should return a monthly summary', function() {
-      var output = basicList.monthlySummary();
-      expect(output.length).to.be(3);
-      expect(output.at(0).get('date')).to.be('1/1');
-      expect(output.at(0).get('amount').get()).to.be(5);
-      expect(output.at(1).get('date')).to.be('3/1');
-      expect(output.at(1).get('amount').get()).to.be(3);
-      expect(output.at(2).get('date')).to.be('11/1');
-      expect(output.at(2).get('amount').get()).to.be(7);
-   });
-
    it('should return a summed total', function() {
       var output = basicList.sumEntries();
       expect(output.get()).to.be(15);
