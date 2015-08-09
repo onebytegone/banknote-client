f2997837a972c02e82e94f738f7280fd7745aa97
Ethan Smith <ethan@onebytegone.com>
2015-08-08 21:04:31 -0400

    Added class to allow running sequences

---
+++ src/model/operations/entries/EntriesBySequence.js
@@ -0,0 +1,34 @@
+var _ = require('underscore'),
+    AmountEntryCollection = require('../../AmountEntryCollection');
+
+var EntriesBySequence = function() { };
+
+EntriesBySequence.prototype = {
+   /**
+    * This calculates a new AmountEntryCollection based on the given sequence
+    * modifier. This will loop through each element in the collection.
+    *
+    * @param entries AmountEntryCollection
+    * @param step function(AmountEntry last, AmountEntry current)->AmountEntry
+    * @param initial AmountEntry
+    * @return AmountEntryCollection
+    */
+   run: function(entries, step, initial) {
+      var collection = new AmountEntryCollection(),
+          collectionList;
+
+      entries.reduce(function(carry, entry) {
+         var forged = step(carry, entry);
+         if (forged) {
+            collection.add(forged);
+            return forged;
+         }
+
+         return entry;
+      }, initial);
+
+      return collection;
+   }
+};
+
+module.exports = EntriesBySequence;
