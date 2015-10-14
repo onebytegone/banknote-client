ce69fb17499c0690c7ad3f4f80030848e64a839d
Ethan Smith <ethan@onebytegone.com>
2015-10-13 19:24:24 -0700

    Disable editable for DifferenceController

--- src/controller/DifferenceController.js
+++ src/controller/DifferenceController.js
@@ -43,7 +43,8 @@ var DifferenceController = ControlBones.extend({
       return new ParticularsModel({
          name: this.title,
          dataset: differenceMonthly,
-         displayType: MonthlySummary
+         displayType: MonthlySummary,
+         editable: false
       });
    }
 });
