3e04aeb6af3805a88054baf58ac741385295d005
Ethan Smith <ethan@onebytegone.com>
2015-11-06 19:05:14 -0500

    Started to refactor controllers

--- src/controller/CategorizedController.js
+++ src/controller/CategorizedController.js
@@ -1,41 +1,82 @@
 var _ = require('underscore'),
+    Backbone = require('backbone'),
     ControlBones = require('./ControlBones'),
+    TableView = require('../view/table/TableView'),
+    TableRow = require('../view/table/TableRow'),
+    TableBody = require('../view/table/TableBody'),
+    TableRowModel = require('../model/table/TableRowModel'),
+    StatementLabelCell = require('../view/table/cell/StatementLabelCell'),
+    AmountEntryCell = require('../view/table/cell/AmountEntryCell'),
+    StatementSumCell = require('../view/table/cell/StatementSumCell'),
+    TableMonthRow = require('../view/table/rows/TableMonthRow'),
+    SummaryBlock = require('../view/SummaryBlock'),
     AmountEntry = require('../model/AmountEntry'),
     AmountEntryCollection = require('../model/AmountEntryCollection'),
-    ParticularsModel = require('../model/ParticularsModel'),
-    StatementCollection = require('../model/StatementCollection'),
     StatementsByCategory = require('../model/operations/statements/StatementsByCategory'),
-    TotalByMonth = require('../model/operations/entries/TotalByMonth'),
-    MonthlyTable = require('../view/table/MonthlyTable');
-
+    TotalByMonth = require('../model/operations/entries/TotalByMonth');
 
 var CategorizedController = ControlBones.extend({
    title: 'Categorized Table',
-   source: 'fieldnamehere',
-   editable: false,
 
-   render: function(data) {
-      var collection = new AmountEntryCollection(_.map(data[this.source], function(note) {
+   parseData: function(rawData) {
+      var collection = new AmountEntryCollection(_.map(rawData, function(note) {
          return new AmountEntry(note);
       }));
 
-      var categorized = (new StatementsByCategory()).run(collection);
-      var monthly = categorized.reduce(function(carry, statement) {
-         statement.set(
-            'entries',
-            (new TotalByMonth()).run(statement.get('entries'))
-         );
-         carry.add(statement);
-         return carry;
-      }, new StatementCollection());
+      return (new StatementsByCategory()).run(collection);
+   },
 
-      return new ParticularsModel({
-         name: this.title,
-         dataset: monthly,
-         displayType: MonthlyTable,
-         editable: this.editable
+   render: function(rawData) {
+      var categorized = this.parseData(rawData);
+          rowData = categorized.at(0);
+
+      var summary = new SummaryBlock({
+         model: new Backbone.Model({
+            'header': this.title
+         })
       });
-   }
+
+      var table = new TableView();
+
+      var body = new TableBody({
+         collection: new Backbone.Collection([
+            new TableRowModel({
+               prepended: rowData,
+               members: (new TotalByMonth()).run(rowData.get('entries')),
+               appended: rowData
+            }),
+            new TableRowModel({
+               prepended: rowData,
+               members: (new TotalByMonth()).run(rowData.get('entries')),
+               appended: rowData
+            })
+         ]),
+         childViewOptions: {
+            prependCellType: StatementLabelCell,
+            cellType: AmountEntryCell,
+            appendCellType: StatementSumCell
+         }
+      });
+
+      var row = new TableMonthRow({
+         prependedModel: new Backbone.Model(),
+         appendedModel: new Backbone.Model({
+            'text': 'Yearly'
+         })
+      });
+
+      summary.on('show', function() {
+         summary.content.show(table);
+      });
+
+      table.on('show', function() {
+         table.header.show(row);
+         table.body.show(body);
+      });
+
+      return summary;
+   },
+
 });
 
 module.exports = CategorizedController;
