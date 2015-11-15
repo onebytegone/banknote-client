400401f0cfa5b438df4273e5095f1068c672b6ef
Ethan Smith <ethan@onebytegone.com>
2015-11-15 08:27:51 -0500

    Added EditableAmountCell

---
+++ src/view/table/cell/EditableAmountCell.js
@@ -0,0 +1,24 @@
+/**
+ * Cell used for showing and allowing the editing of an AmountEntry
+ *
+ * Copyright 2015 Ethan Smith
+ */
+
+var $ = require('jquery'),
+    AmountEntryCell = require('./AmountEntryCell');
+
+var EditableAmountCell = AmountEntryCell.extend({
+   className: function() {
+      return this.model.get('amount').get() === 0 ? 'zero' : '';
+   },
+   _renderContent: function() {
+      var elem = $('<input />').attr({
+         type: 'text',
+         value: this.model.get('amount').readable()
+      });
+
+      return $('<div>').append(elem.clone()).html();
+   }
+});
+
+module.exports = EditableAmountCell;
