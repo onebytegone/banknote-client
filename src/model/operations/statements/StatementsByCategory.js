86de8507fa71429b9faf640a9f596216f4510b14
Ethan Smith <ethan@onebytegone.com>
2015-11-20 20:27:16 -0500

    Limit StatementsByCategory to categoryPreference

--- src/model/operations/statements/StatementsByCategory.js
+++ src/model/operations/statements/StatementsByCategory.js
@@ -1,4 +1,5 @@
-var StatementsByFilter = require('./StatementsByFilter');
+var _ = require('underscore'),
+    StatementsByFilter = require('./StatementsByFilter');
 
 var StatementsByCategory = function() { };
 
@@ -8,11 +9,16 @@ StatementsByCategory.prototype = {
     * based on what category is stored for the entry.
     *
     * @param entries AmountEntryCollection
+    * @param categoryPreference [String]
     * @return StatementCollection
     */
-   run: function(entries) {
+   run: function(entries, categoryPreference) {
+      var shouldUsePreference = categoryPreference && categoryPreference.length > 0;
+
       return (new StatementsByFilter()).run(entries, function(entry) {
-         return entry.get('category');
+         // Only allow categories that are in the preference to be added
+         var category = entry.get('category');
+         return shouldUsePreference && _.indexOf(categoryPreference, category) === -1 ? false : category;
       });
    }
 };
