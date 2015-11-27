722354e5a989b7ba3f78c50a3c0e808c777409ee
Ethan Smith <ethan@onebytegone.com>
2015-11-27 17:32:45 -0500

    Created base PrimaryDisplayController

---
+++ src/controller/PrimaryDisplayController.js
@@ -0,0 +1,32 @@
+/**
+ * Manages the creation and updating of the various
+ * controllers for the layout.
+ *
+ * Copyright 2015 Ethan Smith
+ */
+
+var _ = require('underscore'),
+    Marionette = require('backbone.marionette'),
+    ControlBones = require('./ControlBones');
+
+var PrimaryDisplayController = function(options) {
+   var self = this;
+
+   // Copy all options to object
+   _.each(options, function(value, key) {
+      self[key] = value;
+   });
+};
+
+PrimaryDisplayController.prototype = {
+
+   /**
+    * @param rawData [{...}] - Parsed data to render
+    * @return Marionette.ItemView
+    */
+   render: function(rawData) {
+      return new Marionette.ItemView();
+   }
+};
+
+module.exports = PrimaryDisplayController;
