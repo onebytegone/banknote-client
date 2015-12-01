073ec439d50960f720fc80507aaec796c605c718
Ethan Smith <ethan@onebytegone.com>
2015-12-01 18:23:36 -0500

    Add table and header to EntryListController

--- src/controller/EntryListController.js
+++ src/controller/EntryListController.js
@@ -2,26 +2,66 @@
  * Copyright 2015 Ethan Smith
  */
 
-var Backbone = require('backbone'),
+var _ = require('underscore'),
+    Backbone = require('backbone'),
     ControlBones = require('./ControlBones'),
 
     // Views
-    SummaryBlock = require('../view/SummaryBlock');
+    SummaryBlock = require('../view/SummaryBlock'),
+    TableView = require('../view/table/TableView'),
+    TableRow = require('../view/table/TableRow'),
+    TextCell = require('../view/table/cell/TextCell');
 
 var EntryListController = ControlBones.extend({
    title: 'Entry List',
+   columns: { },
+
+   // Views
+   summaryBlock: null,  // SummaryBlock
+   table: null,  // TableView
 
    /**
     * @param collection AmountEntryCollection
     * @return SummaryBlock
     */
    render: function(collection) {
+      var self = this;
+
       // Assemble summary block
-      var summary = new SummaryBlock({
+      this.summaryBlock = new SummaryBlock({
          model: this._createSummaryModel()
       });
 
-      return summary;
+      this.table = new TableView({
+         collection: null,
+         childViewOptions: {
+            prependCellType: null,
+            cellType: TextCell,
+            appendCellType: null
+         },
+         header: this._generateHeader()
+      });
+
+      this.summaryBlock.on('show', function() {
+        self.summaryBlock.content.show(self.table);
+      });
+
+      return this.summaryBlock;
+   },
+
+   _generateHeader: function() {
+      return new TableRow({
+         model: new Backbone.Model({
+            'members': new Backbone.Collection(
+               _.map(_.keys(this.columns), function(name) {
+                  return { 'text': name };
+               }),
+               Backbone.Model.extend({
+                  'text': ''
+               })
+            )
+         })
+      });
    }
 });
 
