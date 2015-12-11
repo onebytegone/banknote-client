012aa5f0791050f046c6ba5f30c6df474e6178f6
Ethan Smith <ethan@onebytegone.com>
2015-12-11 08:00:24 -0500

    Add basic toolbar item

---
+++ src/view/ToolbarItem.js
@@ -0,0 +1,18 @@
+/**
+ * Creates a toolbar button that is represented by
+ * an icon, e.g. add button.
+ *
+ * Copyright 2015 Ethan Smith
+ */
+
+ var Backbone = require('backbone'),
+     Marionette = require('backbone.marionette');
+
+var ToolbarItem = Marionette.ItemView.extend({
+   template: '<div><a><i class="fa <%- icon %>"></i></a><div>',
+   model: new Backbone.Model({
+      'icon': 'fa-plus'
+   })
+});
+
+module.exports = ToolbarItem;
